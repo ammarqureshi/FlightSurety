65db2382907bb9fa590789924177056a10f93637
ammar <user@Users-MacBook-Pro.local>
2020-08-07 21:05:19 +0100

    fixed nonce issue when running tests

--- truffle.js
+++ truffle.js
@@ -4,11 +4,15 @@ var mnemonic =   'doll source together survey box meat assist green sand conduct
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: '*',
-      gas: 9999999
+      // provider: function() {
+      //   return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+      // },
+      // network_id: '*',
+      // gas: 9999999
+      host: "127.0.0.1",     // Localhost
+      port: 8545,            // Standard Ganache UI port
+      network_id: "*",
+      gas: 4600000
     }
   },
   compilers: {
